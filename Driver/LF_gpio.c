1f2f3872a4a6f5b5835db9fc1a4c2018c6c3c944
qq_51348934 <qq_51348934@noreply.gitcode.com>
2025-05-29 10:38:50 +0800

    修改LED闪烁代码

--- Driver/LF_gpio.c
+++ Driver/LF_gpio.c
@@ -4,13 +4,15 @@ void GPIO_Init(void)
 {
     RCC->APB2ENR |= (1<<2);
     GPIOA->CRL |= ((3<<0) | (2<<2));
-    GPIOA->BSRR |= (1<<0);
+    GPIOA->BSRR = 1;
 }
 
 void LED_Toggle(void)
 {
     Delay_ms(1000);
-    GPIOA->BSRR &= (~(1<<0));
+    GPIOA->BSRR = 0;
+    Delay_ms(1000);
+    GPIOA->BSRR = 1;
 }
 
 
